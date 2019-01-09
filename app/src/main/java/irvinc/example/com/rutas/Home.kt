db26702bb03606c11c8c33b80f1bf606f3a75e59
Irving <i.romerog93@gmail.com>
2019-01-08 22:46:22 -0800

    Commit Inicial

---
+++ app/src/main/java/irvinc/example/com/rutas/Home.kt
@@ -0,0 +1,88 @@
+package irvinc.example.com.rutas
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_bar_home.*
+import kotlinx.android.synthetic.main.content_home.*
+
+class Home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapita) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_camera -> {
+
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onMapReady(googleMap: GoogleMap)
+    {
+        mMap = googleMap
+
+        mMap.uiSettings.isZoomControlsEnabled
+        mMap.uiSettings.isMyLocationButtonEnabled
+
+
+        val sydney = LatLng(32.6278100, -115.4544600)
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.addMarker(MarkerOptions().position(LatLng(32.6278100, -115.4544600)).title("mexicali xd"))
+
+        Toast.makeText(this, "sdfjen", Toast.LENGTH_LONG).show()
+    }
+}
